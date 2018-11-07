8d390486f47f94d2a1dd87d323b530b45f55dd04
Michal <miketopolsky1@interia.pl>
2018-11-07 20:18:33 +0100

    added files

---
+++ creditcard-app/src/main/java/pl/mbal/creditcard/CreditCard.java
@@ -0,0 +1,25 @@
+package pl.mbal.creditcard;
+
+class CreditCard {
+    
+    boolean blocked = false;
+    int limit;
+    
+    public void assignLimit(double money) {
+        this.limit = money;
+    }
+    
+    public double getLimit() {
+        return limit;
+    }
+    
+    public void block()
+    {
+        this.blocked = true;
+    }
+    
+    public boolean isBlocked()
+    {
+        return blocked;
+    }
+}
---
+++ creditcard-app/src/test/java/pl/mbal/creditcard/CreditCardTest.java
@@ -0,0 +1,40 @@
+package pl.mbal.creditcard;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CreditCardTest {
+
+    @Test
+    public void allowAssignLimitToCard() {
+        CreditCard card = new CreditCard();
+
+        card.assignLimit(2000);
+
+        Assert.assertTrue(card.getLimit() == 2000);
+    }
+
+    @Test
+    public void cantWithdrawWhenCantAfford() {
+
+    }
+    
+
+    @Test
+    public void canBlockCard()
+    {
+        CreditCard card = new CreditCard();
+        
+        card.block();
+        
+        Assert.assertTrue(card.isBlocked());
+
+    @Test(expected = WithdrawWhenOverTheLimit.class)
+    public void cantWithdrawWhenOverTheLimit() throws Exception
+    {
+        CreditCard card = new CreditCard();
+        card.assignLimit(200);
+        card.withdraw(400);
+
+    }
+}
